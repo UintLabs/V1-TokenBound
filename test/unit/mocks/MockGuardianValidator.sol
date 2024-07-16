c13a6cda34b536905250418d893e529fc87401a5
Shanmugadevan <shanmugadevan@protonmail.com>
2024-07-16 13:01:25 +0530

    Added RecoveryModule to initliase

--- test/unit/mocks/MockGuardianValidator.sol
+++ test/unit/mocks/MockGuardianValidator.sol
@@ -1,24 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.25;
 
-import { MockValidator as MockValidatorBase } from "module-bases/mocks/MockValidator.sol";
 import { PackedUserOperation } from "module-bases/external/ERC4337.sol";
-import {SignatureChecker} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
-contract MockGuardianValidator is MockValidatorBase {
+import { SignatureChecker } from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import { ValidationData } from "@ERC4337/account-abstraction/contracts/core/Helpers.sol";
+import { IValidator } from "erc7579/interfaces/IERC7579Module.sol";
+
+contract MockGuardianValidator is IValidator {
+    type Validation is uint256;
+
     error GuardianValidator_LengthMismatch();
 
     mapping(address guardian => bool isEnabled) public isGuardianEnabled;
 
+    Validation internal constant VALIDATION_SUCCESS = Validation.wrap(0);
+    Validation internal constant VALIDATION_FAILED = Validation.wrap(1);
+
     function validateUserOp(
-        PackedUserOperation calldata userOp,
-        bytes32 userOpHash
+        PackedUserOperation calldata,
+        /**
+         * userOp
+         */
+        bytes32
     )
         external
-        virtual
         override
-        returns (ValidationData)
+        returns (
+            /**
+             * userOpHash
+             */
+            uint256
+        )
     {
-        return VALIDATION_SUCCESS;
+        return Validation.unwrap(VALIDATION_SUCCESS);
     }
 
     function setGuardian(address[] calldata _guardian, bool[] calldata _isEnabled) external {
@@ -29,4 +43,23 @@ contract MockGuardianValidator is MockValidatorBase {
             isGuardianEnabled[_guardian[i]] = _isEnabled[i];
         }
     }
+
+    function onInstall(bytes calldata data) external override { }
+
+    function onUninstall(bytes calldata data) external override { }
+
+    function isModuleType(uint256 moduleTypeId) external view override returns (bool) { }
+
+    function isInitialized(address smartAccount) external view override returns (bool) { }
+
+    function isValidSignatureWithSender(
+        address sender,
+        bytes32 hash,
+        bytes calldata data
+    )
+        external
+        view
+        override
+        returns (bytes4)
+    { }
 }
